e2f209445f706813af046741228cca6ecf0440bd
krishnapriyapv <kpriyapv20@gmail.com>
2026-03-05 13:48:30 +0530

    monthly roster report - add from and to date

--- thinknxg_kx_v3/thinknxg_kx_v3/report/monthly_roster/monthly_roster.js
+++ thinknxg_kx_v3/thinknxg_kx_v3/report/monthly_roster/monthly_roster.js
@@ -40,7 +40,21 @@ frappe.query_reports["Monthly Roster"] = {
                 return monthNames[today.getMonth()];  // getMonth() returns 0–11
             }()
         },
-		 {
+        {
+            "fieldname": "from_date",
+            "label": "From Date",
+            "fieldtype": "Date",
+            "reqd": 0,
+             "default": ""
+        },
+        {
+            "fieldname": "to_date",
+            "label": "To Date",
+            "fieldtype": "Date",
+            "reqd": 0,
+            "default": ""
+        },
+		{
             "fieldname": "company",
             "label": "Company",
             "fieldtype": "Link",
